2d3c5e0b634f81fee12f7eba4d8907d9a18cc7a6
Arief Syaifuddin <asvelezer@gmail.com>
2021-07-03 21:29:22 +0700

    v0.0.0

---
+++ app.py
@@ -0,0 +1,102 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+import face_recognition as fr
+from setuptools.sandbox import save_modules
+from helper import _dir, _dir_faces, _dir_encoded, compress_img, delete_file, get_pickled_images, list_files, save_pickle, save_file, encode_faces, read_pickle, classify_face
+import time
+from typing import List
+
+
+app = FastAPI(debug=True)
+
+
+@app.get("/")
+def encode_all_images():
+    start = time.perf_counter()
+
+    server_images = list_files(_dir_faces, '.jpg')
+
+    total_faces = len(server_images)
+    number_encoded = total_faces
+    for filename in server_images:
+        encoded_faces = encode_faces('/'.join([_dir_faces, filename]))
+
+        number_faces_detected = len(encoded_faces)
+        if number_faces_detected > 1 or number_faces_detected < 1:
+            number_encoded -= 1
+            continue
+
+        save_pickle('/'.join([_dir_encoded, filename]), encoded_faces[0])
+
+    end = time.perf_counter()
+    return {
+        'status': 'success',
+        'message': f'Encoded {number_encoded} of {total_faces} images',
+        'response_time': end-start
+    }
+
+
+@app.post("/upload/")
+def upload(name: str, file: UploadFile = File(...)):
+    start = time.perf_counter()
+
+    file.filename = '.'.join([name, 'jpg'])
+    save_file(_dir_faces, file.filename, file.file)
+    encoded_faces = encode_faces('/'.join([_dir_faces, file.filename]))
+
+    number_faces_detected = len(encoded_faces)
+    if number_faces_detected > 1:
+        delete_file('/'.join([_dir_faces, file.filename]))
+        return {
+            'status': 'fail',
+            'message': 'Detected more than one faces. Please, upload an image which contain only one face',
+            'response_time': time.perf_counter()-start
+        }
+    if number_faces_detected < 1:
+        delete_file('/'.join([_dir_faces, file.filename]))
+        return {
+            'status': 'fail',
+            'message': 'No face detected',
+            'response_time': time.perf_counter()-start
+        }
+
+    save_pickle(_dir_encoded, file.filename, encoded_faces[0])
+    end = time.perf_counter()
+    return {
+        'status': 'success',
+        'message': 'Image uploaded',
+        "filename": file.filename,
+        'response_time': end-start
+    }
+
+
+@app.post("/find/")
+def find(excludes: List[str] = [], file: UploadFile = File(...)):
+    start = time.perf_counter()
+
+    server_images = list_files(_dir_encoded, '.jpg')
+    encoded_faces = get_pickled_images(server_images)
+    for x in excludes:
+        encoded_faces.pop(x, 'deleted')
+
+    save_file(_dir, 'unknowns.jpg', file.file)
+    compress_img('unknowns.jpg', (200, 200), 30)
+    unknowns = encode_faces('unknowns.jpg')
+
+    number_faces_detected = len(unknowns)
+    if number_faces_detected < 1:
+        return {
+            'status': 'fail',
+            'message': 'No face detected',
+            'response_time': time.perf_counter()-start
+        }
+
+    data = classify_face(unknowns, encoded_faces)
+
+    end = time.perf_counter()
+    return {
+        'status': 'success',
+        'data': data,
+        'excludes': excludes,
+        'response_time': end-start
+    }
---
+++ helper.py
@@ -0,0 +1,108 @@
+import os
+import shutil
+import pickle
+from typing import Dict, List, Tuple
+import face_recognition as fr
+import numpy as np
+from PIL import Image
+
+
+_dir = os.path.dirname(__file__)
+_dir_faces = '/'.join([_dir, 'faces'])
+_dir_encoded = '/'.join([_dir, 'encoded'])
+
+
+def save_file(directory: str, filename: str, content: bytes):
+    with open('/'.join([directory, filename]), 'wb') as buffer:
+        shutil.copyfileobj(content, buffer)
+
+
+def save_pickle(directory: str, filename: str, content):
+    with open('/'.join([directory, filename]), 'wb') as f:
+        pickle.dump(content, f)
+
+
+def read_pickle(filename: str):
+    with open(filename, 'rb') as f:
+        loaded = pickle.load(f)
+        return loaded
+
+
+def delete_file(filename: str):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+
+def list_files(directory: str, extension: str = '.jpg') -> List:
+    for _, _, fnames in os.walk(directory):
+        files = [f for f in fnames if f.endswith(extension)]
+
+    return files
+
+
+# def pickling_image(image):
+#     filename = image.split('/')[-1]
+#     filename = '/'.join([_dir_encoded, filename])
+#     if not os.path.isfile(filename):
+#         try:
+#             content = encode_faces(image)[0]  # encode one face
+#         except IndexError:
+#             print('No face detected')
+#             delete_image(filename.split('/')[-1])
+#         else:
+#             save_pickle(filename, content)
+
+
+def get_pickled_images(images: List) -> Dict:
+    dict = {}
+    for img in images:
+        filename = img.split('/')[-1]  # name.jpg
+        nama = filename.split(".")[0]  # name
+        filename = '/'.join([_dir_encoded, filename])
+        dict[nama] = read_pickle(filename)
+
+    return dict
+
+
+def encode_faces(img_path: str):
+    '''return encoded all face in a image'''
+    face = fr.load_image_file(img_path)
+
+    flocations = fr.face_locations(face, 2)
+    result = fr.face_encodings(face, flocations, model='large')
+
+    return result
+
+
+def compress_img(img_path: str, size: Tuple, quality: int):
+    img = Image.open(img_path)
+    img_size = img.size
+    if img_size[0] > size[0] or img_size[1] > size[1]:
+        img.thumbnail(size, Image.ANTIALIAS)
+    if img.mode == 'RGBA':  # png A for alpha which is transparency
+        img = img.convert('RGB')
+    img.save(img_path, quality=quality)
+
+
+def classify_face(unknown_face_encodings, encoded_faces: Dict):
+    faces_encoded = list(encoded_faces.values())
+    known_face_names = list(encoded_faces.keys())
+
+    data = {
+        'detected': [],
+        'distances': [],
+        'nearest': []
+    }
+    for face_encoding in unknown_face_encodings:
+        face_distances = fr.face_distance(faces_encoded, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        nearest = known_face_names[best_match_index]
+        if face_distances[best_match_index] <= 0.62:
+            name = nearest
+        else:
+            name = "Unknown"
+        data['detected'].append(name)
+        data['distances'].append(min(face_distances))
+        data['nearest'].append(nearest)
+
+    return data
